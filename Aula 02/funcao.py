96b89f8de20deba12a1aa1c9c05efd381ab2d477
Rômulo Lopes Gonçalves <romulo.08g@gmail.com>
2025-02-13 20:29:01 -0300

    aula 2 inicio
    inicio aula 2

---
+++ Aula 02/funcao.py
@@ -0,0 +1,20 @@
+def analise_positivo(numero):
+    if(numero>-1):
+        return "positivo"
+    else:
+        return "negativo"
+    
+def par_impar(numero):
+    if(numero % 2 == 0):
+        return "par"
+    else:
+        return "impar"
+
+
+numero = int(input("Digite um numero para ser analisado"))
+
+positivo_ou_negativo = analise_positivo(numero)
+positivo_ou_negativo = par_impar(numero)
+
+print(f"o numero é {str(positivo_ou_negativo)} e é {positivo_ou_negativo}")
+  
