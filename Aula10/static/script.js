4750cd1ad10598ab353c1880d42dfd5c37f672c7
ROMULO LOPES GONCALVES <romulo.lgoncalves@senacsp.edu.br>
2025-03-27 21:11:21 -0300

    dice

--- Aula10/static/script.js
+++ Aula10/static/script.js
@@ -5,6 +5,7 @@ function adicionarAtaque() {
     let ataqueContainer = document.createElement("li");
 
     ataqueContainer.innerHTML = `
+        
         <label><strong>Arma:</strong></label>
         <input type="text" name="ataques[\${index}][arma]" required><br>
 
@@ -14,7 +15,7 @@ function adicionarAtaque() {
         <label><strong>Dado:</strong></label>
         <input type="number" name="ataques[\${index}][dado]" required><br>
 
-        <button type="button" onclick="removerAtaque(this)">Remover</button>
+        <button class="botao botao_mesa" type="button" onclick="removerAtaque(this)">Remover</button>
     `;
 
     listaAtaques.appendChild(ataqueContainer);
@@ -22,4 +23,20 @@ function adicionarAtaque() {
 
 function removerAtaque(botao) {
     botao.parentElement.remove();
+}
+
+function calcularAtaque() {
+    // Pega os valores do dado e bônus
+    let ladosDado = document.getElementById("ladosDado").value;
+    let bonus = document.getElementById("bonosDado").value;
+
+    // Gera um número aleatório de 1 até o valor de ladosDado
+    let valorDado = Math.floor(Math.random() * ladosDado) + 1;
+
+    // Calcula o resultado
+    let resultado = parseInt(valorDado) + parseInt(bonus);
+
+    // Exibe o resultado no elemento <span>
+    let resultadoSpan = document.getElementById("resultado");
+    resultadoSpan.innerHTML = `${valorDado} + ${bonus} = ${resultado}`;
 }
